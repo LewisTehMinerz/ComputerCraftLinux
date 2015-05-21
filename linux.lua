04f0857011eda4581e42936ecfc529a493ae9619
LewisTehMinerz <lewisminecraft@hotmail.co.uk>
2015-05-21 18:29:20 +0100

    Update linux.lua

--- linux.lua
+++ linux.lua
@@ -2,9 +2,9 @@ term.clear()
 term.setCursorPos( 1,1 )
 write( "CCLinux 0.1 load... " )
 if not fs.exists( ".linuxCC" ) then
-  term.setForegroundColor( colors.red )
+  term.setTextColor( colors.red )
   write( "[ err ]" )
-  term.setForegroundColor( colors.white )
+  term.setTextColor( colors.white )
   printError( "[ CCLINUX ] An error occured while trying to load CCLinux. A log file ( \"cclinuxerr.log\" ) has been created." )
   i = fs.open( "cclinuxerr.log", "w" )
   i.write( "[ CCLinux ] Ver = 0.1" )
@@ -13,7 +13,7 @@ if not fs.exists( ".linuxCC" ) then
   i.close()
   error( "[ CCLinux ] TERMINATED", 0 )
 else
-  term.setForegroundColor( colors.green )
+  term.setTextColor( colors.green )
   write( "[ ok ]" )
-  term.setForegroundColor( colors.white )
+  term.setTextColor( colors.white )
 end
